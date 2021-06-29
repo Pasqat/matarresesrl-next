23b0d26a120fe80ec7d817a0ed23bce532a0aedc
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-06-29 19:32:54 +0200

    add blog, [slug], api

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -48,7 +48,7 @@ export default function Post({ postData }) {
                 dangerouslySetInnerHTML={{ __html: postData.content }}
               />
             </article>
-          </>
+          </
         )}
         <p>
           <Link href="/blog">
