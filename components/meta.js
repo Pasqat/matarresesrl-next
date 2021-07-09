d8008d47e615857d2d10d7d7e696daec64648469
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-07-09 20:49:59 +0200

    add favicon

--- components/meta.js
+++ components/meta.js
@@ -3,40 +3,96 @@ import Head from "next/head";
 export default function Meta() {
   return (
     <Head>
-    {/* TODO: generate and add the icons */}
+      {/* TODO: generate and add the icons */}
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/img/apple-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/img/apple-icon-60x60.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/img/apple-icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/img/apple-icon-76x76.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/img/apple-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/img/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/img/apple-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/img/apple-icon-152x152.png"
+      />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="/img/apple-icon-180x180.png"
+      />
+      {/* TODO: what is the safari-pinned-tab? */}
+      {/* <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg"
+        color="#000000"
+      /> */}
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/img/android-icon-192x192.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href="/img/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/img/favicon-96x96.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href="/img/favicon-16x16.png"
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
+
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <meta name="msapplication-TileColor" content="#e0851f" />
+      <meta name="msapplication-config" content="/img/browserconfig.xml" />
+      <meta name="theme-color" content="#e0851f" />
+      {/* <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`Traduttori di idee, creatori di spazi`}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      {/*   TODO: what is og:image???     */}
+      {/* <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
+      <link rel="manifest" href="/img/manifest.json" />
+      <meta name="msapplication-TileImage" content="/img/ms-icon-144x144.png" />
     </Head>
   );
 }
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,88 +1,89 @@
-import { useRouter } from 'next/router'
-import Head from 'next/head'
-import Link from 'next/link'
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Link from "next/link";
+import Container from "../../components/Container";
+import Layout from "../../components/Layout";
+import Navbar from "../../components/Navbars/Navbar";
 
-import {
-  getAllPostsWithSlug,
-  getPost,
-  getPostAndMorePosts,
-} from '../../lib/api'
-import Navbar from '../../components/Navbars/Navbar'
+import { getAllPostsWithSlug, getPostAndMorePosts } from "../../lib/api";
 
-import { formatDate } from '../../actions/utils/formatDate'
+import { formatDate } from "../../actions/utils/formatDate";
 
-export default function Post({ postData }) {
-  const router = useRouter()
+export default function Post({ postData, posts }) {
+  const router = useRouter();
+  const morePosts = posts?.edges;
 
   if (!router.isFallback && !postData.slug) {
-    console.log('sdeng 💣️')
-    return <p>hmm...sembra ci sia un errore</p>
+    console.log("sdeng 💣️");
+    return <p>hmm...sembra ci sia un errore</p>;
   }
 
   return (
-    <>
-      <Head>
-        {router.isFallback ? (
-          // NOTE: check isFallback is extremly important!!!
-          // https://michaelfulton.co/posts/export-encountered-errors
-          <title>Matarrese srl</title>
-        ) : (
-          <title>{postData.title}</title>
-        )}
-      </Head>
-      <Navbar />
-      <div className="container">
-        <main>
+    <Layout>
+      <Container>
+        <Head>
           {router.isFallback ? (
-            <h2>Loading...</h2>
+            // NOTE: check isFallback is extremly important!!!
+            // https://michaelfulton.co/posts/export-encountered-errors
+            <title>Matarrese srl</title>
           ) : (
-            <>
-              <img
-                style={{ maxWidth: '80vw' }}
-                src={postData.featuredImage?.node.sourceUrl}
-                alt={postData.title}
-              />
-              <article>
-                <div>
-                  <h1>{postData.title}</h1>
-                  <p>pubblicato il {formatDate(postData.date)}</p>
-                </div>
-                <div
-                  className="prose-sm prose sm:prose lg:prose-lg xl:prose-xl prose-yellow"
-                  dangerouslySetInnerHTML={{ __html: postData.content }}
-                />
-              </article>
-            </>
+            <title>{postData.title}</title>
           )}
-          <p>
-            <Link href="/blog">
-              <a>torna agli articoli</a>
-            </Link>
-          </p>
-        </main>
-      </div>
-    </>
-  )
+        </Head>
+        <Navbar />
+        <div className="container">
+          <main>
+            {router.isFallback ? (
+              <h2>Loading...</h2>
+            ) : (
+              <>
+                <img
+                  style={{ maxWidth: "80vw" }}
+                  src={postData.featuredImage?.node.sourceUrl}
+                  alt={postData.title}
+                />
+                <article>
+                  <div>
+                    <h1>{postData.title}</h1>
+                    <p>pubblicato il {formatDate(postData.date)}</p>
+                  </div>
+                  <div
+                    className="prose-sm prose sm:prose lg:prose-lg xl:prose-xl prose-yellow"
+                    dangerouslySetInnerHTML={{ __html: postData.content }}
+                  />
+                </article>
+              </>
+            )}
+            <p>
+              <Link href="/blog">
+                <a>torna agli articoli</a>
+              </Link>
+            </p>
+          </main>
+        </div>
+      </Container>
+    </Layout>
+  );
 }
 
 export async function getStaticProps({ params, preview = false }) {
-  const data = await getPostAndMorePosts(params.slug, preview)
+  const data = await getPostAndMorePosts(params.slug, preview);
 
-  console.log(data)
+  console.log(data);
   return {
     props: {
       preview,
       postData: data.post,
       posts: data.posts,
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const allPosts = await getAllPostsWithSlug()
+  const allPosts = await getAllPostsWithSlug();
 
   return {
     paths: allPosts.edges.map(({ node }) => `/blog/${node.slug}`) || [],
     fallback: true,
-  }
+  };
 }
