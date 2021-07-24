c8a3087b27198d582ade39338b7fccd7bb7a800d
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-07-24 16:48:25 +0200

    fixed next/image issue adding correct domain

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   images: {
     // TODO: cdn wp domain can be other than this. Find a better way.
     // at today 07/13/2021 next don't accept wildcards
-    domains: ["localhost", "matarrese.it", "i1.wp.com", "i0.wp.com", "i2.wp.com" ],
+    domains: ["localhost", "www.matarrese.it"],
   },
 };
