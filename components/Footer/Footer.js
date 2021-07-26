5d1e69bb839718468849b54ad44db0139b1aec23
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-07-26 17:15:50 +0200

    update link

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
-import logoEccelsa from "../../public/img/logos/Eccelsa-RGB_400x400.png"
-import logoQucino from "../../public/img/logos/MARCHIO-QUCINO150.png"
-import logoAliGroup from "../../public/img/logos/Ali_Group_logo.png"
+import logoEccelsa from "../../public/img/logos/Eccelsa-RGB_400x400.png";
+import logoQucino from "../../public/img/logos/MARCHIO-QUCINO150.png";
+import logoAliGroup from "../../public/img/logos/Ali_Group_logo.png";
 
 export default function Footer() {
   return (
@@ -73,45 +73,39 @@ export default function Footer() {
           </div>
           <div className="flex flex-wrap items-end justify-between h-full mt-16">
             {/* TODO: link to Qucino */}
-            <Link href="https://www.istitutoeccelsa.it">
-              <a target="_blank">
-                <div className="relative w-16 h-20">
-                  <Image
-                    placeholder="blur"
-                    layout="fill"
-                    objectFit="contain"
-                    alt="Logo Istituto Eccelsa"
-                    src={logoEccelsa}
-                  />
-                </div>
-              </a>
-            </Link>
-            <Link href="/#">
-              <a>
-                <div className="relative w-24 h-20">
-                  <Image
-                    layout="fill"
-                    objectFit="contain"
-                    alt="Logo Qucino"
-                    src={logoQucino}
-                    placeholder="blur"
-                  />
-                </div>
-              </a>
-            </Link>
-            <Link href="/#">
-              <a>
-                <div className="relative w-16 h-16">
-                  <Image
-                    layout="fill"
-                    objectFit="contain"
-                    alt="Logo Aligroup"
-                    src={logoAliGroup}
-                    placeholder="blur"
-                  />
-                </div>
-              </a>
-            </Link>
+            <a href="https://www.istitutoeccelsa.it" target="_blank">
+              <div className="relative w-16 h-20">
+                <Image
+                  placeholder="blur"
+                  layout="fill"
+                  objectFit="contain"
+                  alt="Logo Istituto Eccelsa"
+                  src={logoEccelsa}
+                />
+              </div>
+            </a>
+            <a href="https://www.qucino.it/" target="_blank">
+              <div className="relative w-24 h-20">
+                <Image
+                  layout="fill"
+                  objectFit="contain"
+                  alt="Logo Qucino"
+                  src={logoQucino}
+                  placeholder="blur"
+                />
+              </div>
+            </a>
+            <a>
+              <div className="relative w-16 h-16">
+                <Image
+                  layout="fill"
+                  objectFit="contain"
+                  alt="Logo Aligroup"
+                  src={logoAliGroup}
+                  placeholder="blur"
+                />
+              </div>
+            </a>
           </div>
         </div>
         <div className="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
@@ -141,10 +135,11 @@ export default function Footer() {
                 </Link>
               </li>
               <li>
-                {/* TODO: add terms and conditions */}
-                <a className="text-gray-600 hover:text-gray-800">
-                  Termini e Condizioni
-                </a>
+                <Link href="/privacy-policy">
+                  <a className="text-gray-600 hover:text-gray-800">
+                    Termini e Condizioni
+                  </a>
+                </Link>
               </li>
             </nav>
           </div>
@@ -234,73 +229,77 @@ export default function Footer() {
             <p className="text-sm text-center text-gray-500">
               by <span className="font-semibold">Pasquale Matarrese</span>
             </p>
-            <Link href="https://www.facebook.com/pasqat">
-              <a className="ml-3 text-gray-500 cursor-pointer hover:text-blue-800">
-                <svg
-                  fill="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-            </Link>
-            <Link href="https://twitter.com/pasqat">
-              <a className="ml-3 text-gray-500 hover:text-blue-500">
-                <svg
-                  fill="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-            </Link>
-            <Link href="https://github.com/pasqat/">
-              <a className="ml-3 text-gray-500 hover:text-black">
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="0"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 
+            <a
+              href="https://www.facebook.com/pasqat"
+              className="ml-3 text-gray-500 cursor-pointer hover:text-blue-800"
+            >
+              <svg
+                fill="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </svg>
+            </a>
+            <a
+              href="https://twitter.com/pasqat"
+              className="ml-3 text-gray-500 hover:text-blue-500"
+            >
+              <svg
+                fill="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+              </svg>
+            </a>
+            <a
+              href="https://github.com/pasqat/"
+              className="ml-3 text-gray-500 hover:text-black"
+            >
+              <svg
+                fill="currentColor"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="0"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 
                       1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 
                       1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 
                       5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-                  />
-                </svg>
-              </a>
-            </Link>
-            <Link href="https://linkedin.com/in/pasquale.matarrese">
-              <a className="ml-3 text-gray-500 hover:text-blue-800">
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="0"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="none"
-                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                  ></path>
-                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                </svg>
-              </a>
-            </Link>
+                />
+              </svg>
+            </a>
+            <a
+              href="https://linkedin.com/in/pasquale.matarrese"
+              className="ml-3 text-gray-500 hover:text-blue-800"
+            >
+              <svg
+                fill="currentColor"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="0"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="none"
+                  d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                ></path>
+                <circle cx="4" cy="4" r="2" stroke="none"></circle>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
