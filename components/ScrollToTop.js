3f29a0682f567fbd0cfedb8210c3be911b373d25
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-07-26 14:30:35 +0200

    scroll to top like a pro

---
+++ components/ScrollToTop.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    isVisible && (
+      <div
+        className="fixed w-8 h-8 bg-yellow-600 rounded cursor-pointer bottom-10 right-4 shadow-md items-end flex justify-center"
+        onClick={scrollToTop}
+      >
+        <svg
+          className="w-5 h-5 text-white animate-bounce mb-1"
+          viewBox="0 0 10 20"
+          fill="none"
+          stroke-linecap="round"
+          stroke-width="2"
+          stroke-linejoin="round"
+          fill="none"
+          stroke="currentColor"
+        >
+          <path d="M1 5L5 1M5 1L9 5M5 1V19"/>
+        </svg>
+      </div>
+    )
+  );
+};
+
+export default ScrollToTop;
--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,15 @@ import "../styles/index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { ApolloProvider } from "@apollo/client";
 
+import ScrollToTop from "../components/ScrollToTop";
+
 import client from "../lib/apolloClient";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
+      <ScrollToTop/>
     </ApolloProvider>
   );
 }
