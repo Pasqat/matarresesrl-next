8f461e9d264c57eb1f8ee60c1811cdb0a22e7af7
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-09-10 20:06:43 +0200

    clean up

--- pages/eventi/index.js
+++ pages/eventi/index.js
@@ -1,13 +1,11 @@
 import { getEvents } from "../../lib/api";
-import Link from "next/link";
 
 import Layout from "../../components/Layout";
 import Container from "../../components/Container";
-import Date from "../../components/Date";
 import Header from "../../components/Header/Header";
 import CardEvent from "../../components/Card/CardEvent";
 
-import { formatDate, getMonth, getDayNumeric } from "../../actions/utils/formatDate";
+import { getMonth } from "../../actions/utils/formatDate";
 import Head from "next/head";
 
 function GroupByMonth({ data }) {
@@ -40,76 +38,6 @@ function GroupByMonth({ data }) {
                 venue={event.venue}
                 coverImage={event.featuredImage?.node}
               />
-              {/* < div className="overflow-hidden bg-white border-2 border-gray-200 rounded-lg border-opacity-60 shadow-lg" >
-                <div className="relative flex items-center justify-center min-h-[10rem] text-center bg-yellow-600">
-                  <h3 className="relative z-10 px-2 text-white overflow-hidden font-serif text-4xl font-extrabold leading-tight lg:px-10">
-                    <Link href={`/eventi/${event.id}`}>
-                      <a
-                        className="hover:underline"
-                        dangerouslySetInnerHTML={{ __html: event.title }}
-                      ></a>
-                    </Link>
-                  </h3>
-                  <div className="leading-tight select-none absolute z-0 text-7xl transform md:scale-[5] scale-[3.5] text-gray-100 opacity-30 font-extrabold font-serif">
-                    {getDay(event.startDate)}
-                  </div>
-                </div>
-                <div className="flex flex-col justify-between p-6">
-                  <div>
-                    {formatDate(event.startDate) ===
-                      formatDate(event.endDate) ? (
-                      <Date
-                        dateString={event.startDate}
-                        className="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font"
-                      />
-                    ) : (
-                      <>
-                        <Date
-                          dateString={event.startDate}
-                          className="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font"
-                        />
-                        {" - "}
-                        <Date
-                          dateString={event.endDate}
-                          className="mb-1 text-xs font-medium tracking-widest text-gray-400 title-font"
-                        />
-                      </>
-                    )}
-                    <div
-                      className="mb-3 leading-relaxed"
-                      dangerouslySetInnerHTML={{
-                        __html:
-                          event.excerpt?.length > 160
-                            ? event.excerpt?.substr(0, 160) + "..."
-                            : event.excerpt,
-                      }}
-                    ></div>
-                    <div className="text-right font-bold">{event.cost ? `${event.cost} €` : "gratis"}</div>
-                    <div className="flex flex-wrap items-center ">
-                      <Link href={`/eventi/${event.id}`}>
-                        <a className="z-2 inline-flex items-center text-yellow-500 md:mb-2 lg:mb-0">
-                          maggiori dettagli
-                          <svg
-                            className="w-4 h-4 ml-2 animate-bounceX"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            <path d="M5 12h14"></path>
-                            <path d="M12 5l7 7-7 7"></path>
-                          </svg>
-                        </a>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              {/* <div>{event.organizers?.nodes.title}</div> */}
-
             </div>
           ) : null
         )
@@ -139,6 +67,7 @@ export default function Events({ data }) {
           content="I prossimi eventi che si terranno a cura di Matarrese srl. Approfondimenti, master class, demo ed altro"
         />
         <meta property="og:url" content="https://www.matarrese.it/eventi" />
+
       </Head>
       <Layout className="bg-gray-100">
         <div className="mb-24">
