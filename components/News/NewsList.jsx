7aebb36e3b39ecacf3238787e100db9197b8cd82
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-10-30 11:45:42 +0200

    minor refactor

--- components/News/NewsList.jsx
+++ components/News/NewsList.jsx
@@ -1,10 +1,35 @@
-import CardPost from "../Card/CardPost";
-import HeroPost from "./HeroPost";
+import CardPost from '../Card/CardPost'
+import HeroPost from './HeroPost'
 
 // Function to update the query with the new results
-const updateQuery = (previousResult, { fetchMoreResult }) => {
-  return fetchMoreResult.posts.edges.length ? fetchMoreResult : previousResult;
-};
+const updateQuery = (previousResult, {fetchMoreResult}) => {
+  return fetchMoreResult.posts.edges.length ? fetchMoreResult : previousResult
+}
+
+function CardPlaceholder() {
+  return (
+    <div className="h-full overflow-hidden ">
+      <div className="animate-pulse">
+        <div className="w-full h-64 md:h-36 lg:h-48 bg-gray-300" />
+        <div className="flex flex-col justify-between p-6 space-y-4">
+          <div className="space-y-2">
+            <div className="mb-2 bg-gray-300 h-4 rounded w-1/3" />
+            <div className="bg-gray-300 h-5 w-full rounded" />
+            <div className="bg-gray-300 h-5 w-5/6 rounded" />
+            <div className="hidden md:block bg-gray-300 h-5 w-full rounded" />
+          </div>
+          <div className="space-y-2">
+            <div className="bg-gray-300 h-4 w-full rounded" />
+            <div className="bg-gray-300 h-4 w-4/6 rounded" />
+            <div className="hidden md:block bg-gray-300 h-4 w-5/6 rounded" />
+            <div className="hidden md:block bg-gray-300 h-4 w-full rounded" />
+          </div>
+          <div className="bg-yellow-300 h-4 w-4/6 rounded" />
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default function NewsList({
   posts,
@@ -22,10 +47,10 @@ export default function NewsList({
         </p>
         <p>{JSON.stringify(error)}</p>
       </div>
-    );
+    )
   }
 
-  const newsPlaceholder = Array(9).fill(null);
+  const newsPlaceholder = Array(9).fill(null)
 
   if (loading)
     return (
@@ -34,35 +59,16 @@ export default function NewsList({
           <div className="flex flex-wrap -m-4">
             {newsPlaceholder.map((_, i) => (
               <div className="p-4 w-full md:w-1/3" key={i}>
-                <div className="h-full overflow-hidden ">
-                  <div className="animate-pulse">
-                    <div className="w-full h-64 md:h-36 lg:h-48 bg-gray-300" />
-                    <div className="flex flex-col justify-between p-6 space-y-4">
-                      <div className="space-y-2">
-                        <div className="mb-2 bg-gray-300 h-4 rounded w-1/3" />
-                        <div className="bg-gray-300 h-5 w-full rounded" />
-                        <div className="bg-gray-300 h-5 w-5/6 rounded" />
-                        <div className="hidden md:block bg-gray-300 h-5 w-full rounded" />
-                      </div>
-                      <div className="space-y-2">
-                        <div className="bg-gray-300 h-4 w-full rounded" />
-                        <div className="bg-gray-300 h-4 w-4/6 rounded" />
-                        <div className="hidden md:block bg-gray-300 h-4 w-5/6 rounded" />
-                        <div className="hidden md:block bg-gray-300 h-4 w-full rounded" />
-                      </div>
-                      <div className="bg-yellow-300 h-4 w-4/6 rounded" />
-                    </div>
-                  </div>
-                </div>
+                <CardPlaceholder key={i} />
               </div>
             ))}
           </div>
         </div>
       </section>
-    );
+    )
 
-  const heroPost = posts.edges[0]?.node;
-  const morePosts = posts.edges.slice(1);
+  const heroPost = posts.edges[0]?.node
+  const morePosts = posts.edges.slice(1)
 
   return (
     <>
@@ -80,7 +86,7 @@ export default function NewsList({
                 />
               )}
               <div className="flex flex-wrap -m-4">
-                {morePosts.map(({ node }) => {
+                {morePosts.map(({node}) => {
                   return (
                     <div key={node.title} className="p-4 md:w-1/3">
                       <CardPost
@@ -91,7 +97,7 @@ export default function NewsList({
                         excerpt={node.excerpt}
                       />
                     </div>
-                  );
+                  )
                 })}
               </div>
             </div>
@@ -109,7 +115,7 @@ export default function NewsList({
                     before: posts.pageInfo.startCursor || null,
                   },
                   updateQuery,
-                });
+                })
               }}
             >
               <i className="fas fa-arrow-left" />
@@ -126,7 +132,7 @@ export default function NewsList({
                     before: null,
                   },
                   updateQuery,
-                });
+                })
               }}
             >
               <i className="fas fa-arrow-right" />
@@ -137,5 +143,5 @@ export default function NewsList({
         <div>Nessuna news trovata...</div>
       )}
     </>
-  );
+  )
 }
