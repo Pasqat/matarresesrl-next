d6f417e6aa7c02cae2a3bf416886f1a7f755387e
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2022-03-21 18:40:24 +0100

    fix social share bar

--- components/SocialShareBar/SocialShareBar.jsx
+++ components/SocialShareBar/SocialShareBar.jsx
@@ -1,3 +1,7 @@
+import {FacebookIcon} from '../icons/facebook-icon'
+import {LinkedInIcon} from '../icons/linkedin-icon'
+import {MailIcon} from '../icons/mail-icon'
+import {TwitterIcon} from '../icons/twitter-icon'
 import SocialShareButton from '../SocialShareButton/SocialShareButton'
 
 const domainUrl = process.env.NEXT_PUBLIC_WP_API_URL
@@ -11,19 +15,27 @@ export default function SocialShareBar({route, title}) {
       <div className="sticky top-28 z-3 mt-8 flex h-full flex-row items-center justify-center lg:ml-10 lg:mt-0 lg:flex-col">
         <SocialShareButton
           href={`https://www.facebook.com/sharer/sharer.php?u=${domainUrl}${route}`}
-          icon="fab fa-facebook"
+          // icon="fab fa-facebook"
+          icon={<FacebookIcon size="60" />}
+          tooltip="su Facebook"
         />
         <SocialShareButton
           href={`https://twitter.com/intent/tweet?text=${title}&url=${domainUrl}${route}`}
-          icon="fab fa-twitter"
+          // icon="fab fa-twitter"
+          icon={<TwitterIcon size="50" />}
+          tooltip="su Twitter"
         />
         <SocialShareButton
           href={`https://www.linkedin.com/sharing/share-offsite/?url=${domainUrl}${route}`}
-          icon="fab fa-linkedin"
+          // icon="fab fa-linkedin"
+          icon={<LinkedInIcon size="56" />}
+          tooltip="su LinkedIn"
         />
         <SocialShareButton
           href={`mailto:?subject=${title}&body=Ciao, ho pensato potesse interessarti: ${domainUrl}${route}`}
-          icon="fas fa-envelope"
+          // icon="fas fa-envelope"
+          icon={<MailIcon size="50" />}
+          tooltip="via mail"
         />
       </div>
     </>
--- components/SocialShareButton/SocialShareButton.jsx
+++ components/SocialShareButton/SocialShareButton.jsx
@@ -1,20 +1,22 @@
-// import Tooltip from "../Tooltip/Tooltip";
+import Tooltip from '../Tooltip/Tooltip'
 
 /**
- * @param {string} icon - FontAwesome icon name, eg. fab fa-facebook
  * @param children - maybe usefull for tooltip
  */
 export default function SocialShareButton(props) {
-  const {href, icon, children} = props
+  const {href, icon, children, tooltip} = props
   return (
     <a
       href={href}
       target="_blank"
       rel="noreferrer"
-      className="mr-4 lg:mr-0 lg:mb-6"
+      className="mr-4 hover:text-yellow-500 lg:mr-0 lg:mb-6"
     >
-      <i className={`text-3xl text-gray-600 hover:text-yellow-500 ${icon}`} />
-      {children}
+      <Tooltip content={`condividi ${tooltip}`}>
+        <div>{icon}</div>
+        {/* <i className={`text-3xl text-gray-600 hover:text-yellow-500 ${icon}`} /> */}
+        {children}
+      </Tooltip>
     </a>
   )
 }
