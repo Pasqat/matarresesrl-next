6c478e2ff4bc14a79f279b887460f789dadbcd85
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2022-04-04 08:57:33 +0200

    add new domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ module.exports = {
   images: {
     // TODO: cdn wp domain can be other than this. Find a better way.
     // at today 07/13/2021 next don't accept wildcards
-    domains: ['localhost', 'www.matarrese.it', 'matarrese.it'],
+    domains: [
+      'localhost',
+      'www.matarrese.it',
+      'matarrese.it',
+      'be.matarrese.it',
+    ],
   },
   async redirects() {
     return [
