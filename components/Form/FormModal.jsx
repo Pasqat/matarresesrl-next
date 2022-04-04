5515bccc11f9f1e06424ed4c135dcc005eb74899
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2022-04-04 17:16:28 +0200

    not a button

--- components/Form/FormModal.jsx
+++ components/Form/FormModal.jsx
@@ -159,14 +159,24 @@ export default function FormModal({
           {buttonText}
         </Button>
       ) : (
-        <LinkButton
-          variant={variant}
+        <a
           onClick={openModal}
-          withArrow
-          className="mt-4 text-lg"
+          className="text-accent group mt-4 inline-flex cursor-pointer items-center text-lg no-underline md:mb-2 lg:mb-0"
         >
           {buttonText}
-        </LinkButton>
+          <svg
+            className="ml-2 h-4 w-4 animate-bounceX"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M5 12h14"></path>
+            <path d="M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
       )}
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog
