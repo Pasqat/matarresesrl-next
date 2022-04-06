02796ff3e24dbf1705bf8f2b0c4a2252b71881ad
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2022-04-06 15:50:50 +0200

    gTag manager

--- pages/_app.js
+++ pages/_app.js
@@ -18,13 +18,25 @@ function MyApp({Component, pageProps}) {
         id="tag-manager"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-00G4B92CP8');
-            `,
+          __html: (function (w, d, s, l, i) {
+            w[l] = w[l] || []
+            w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'})
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != 'dataLayer' ? '&l=' + l : ''
+            j.async = true
+            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+            f.parentNode.insertBefore(j, f)
+          })(window, document, 'script', 'dataLayer', 'GTM-5M9RQ4B'),
         }}
+        // dangerouslySetInnerHTML={{
+        //   __html: `
+        //       window.dataLayer = window.dataLayer || [];
+        //       function gtag(){dataLayer.push(arguments);}
+        //       gtag('js', new Date());
+        //       gtag('config', 'G-00G4B92CP8');
+        //     `,
+        // }}
       />
       <Component {...pageProps} />
       <ScrollToTop />
