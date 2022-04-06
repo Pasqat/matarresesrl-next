b5da29efff7dcb2c2159e38df6bad1fa67d49e32
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2022-04-06 20:23:56 +0200

    now it should work properly

--- pages/_app.js
+++ pages/_app.js
@@ -31,10 +31,6 @@ function MyApp({Component, pageProps}) {
 
   return (
     <ApolloProvider client={client}>
-      {/* <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-00G4B92CP8"
-      /> */}
       {/* Global Site Code Pixel - Facebook Pixel */}
       <Script
         id="fb-pixel"
@@ -57,16 +53,13 @@ function MyApp({Component, pageProps}) {
         id="tag-manager"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `(function (w, d, s, l, i) {
-            w[l] = w[l] || []
-            w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'})
-            var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s),
-              dl = l != 'dataLayer' ? '&l=' + l : ''
-            j.async = true
-            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
-            f.parentNode.insertBefore(j, f)
-          })(window, document, 'script', 'dataLayer', ${GTM_ID})`,
+          __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}')
+              `,
         }}
       />
       <Component {...pageProps} />
