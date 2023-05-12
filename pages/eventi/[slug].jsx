f6425e5810be47fd6247975e9029844decd90292
Pasquale <pasquale.matarrese@gmail.com>
2023-05-12 11:36:45 +0200

    fix invalid date problem
    Don't know why but the event/[slug] page was full of old code that make it impossible to show the correct dates.

--- pages/eventi/[slug].jsx
+++ pages/eventi/[slug].jsx
@@ -1,25 +1,25 @@
-import {useRouter} from 'next/router'
-import Head from 'next/head'
-import Link from 'next/link'
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Link from "next/link";
 
-import Layout from '../../components/Layout'
-import Header from '../../components/Header/Header'
-import EventBody from '../../components/Events/event-body'
-import Date from '../../components/Date'
-import HeaderBig from '../../components/Header/HeaderBig'
-import FormModal from '../../components/Form/FormModal'
-import SocialShareBar from '../../components/SocialShareBar/SocialShareBar'
-import {H2, H3, Paragraph} from '../../components/typography'
+import Layout from "../../components/Layout";
+import Header from "../../components/Header/Header";
+import EventBody from "../../components/Events/event-body";
+import Date from "../../components/Date";
+import HeaderBig from "../../components/Header/HeaderBig";
+import FormModal from "../../components/Form/FormModal";
+import SocialShareBar from "../../components/SocialShareBar/SocialShareBar";
+import { H2, H3, Paragraph } from "../../components/typography";
 
-import {getAllEventsWithSlug, getEvent} from '../../lib/query/event'
-import {getHour} from '../../actions/utils/formatDate'
-import {SeoDataSection} from '../../components/sections/seodata-section'
+import { getAllEventsWithSlug, getEvent } from "../../lib/query/event";
+import { getHour } from "../../actions/utils/formatDate";
+import { SeoDataSection } from "../../components/sections/seodata-section";
 
-export default function Events({event}) {
-  const router = useRouter()
+export default function Events({ event }) {
+  const router = useRouter();
 
   if (!router.isFallback && !event.slug) {
-    return <p>hmm...sembra ci sia un errore</p>
+    return <p>hmm...sembra ci sia un errore</p>;
   }
 
   // take n String arguments and join them
@@ -33,127 +33,82 @@ export default function Events({event}) {
 
   //   return parsedAddress.join('+')
   // }
+  //
+  console.log("event ->", event);
 
   return (
     <Layout navbarTransparent>
       {/* <Layout > */}
-      {router.isFallback ? (
-        <>
-          <Header href="/eventi">Eventi</Header>
-          <Head>
-            <title>Matarrese srl | Eventi</title>
-          </Head>
-          <main>
-            <H2 variant="secondary">Caricamento di tutti gli eventi</H2>
-          </main>
-        </>
-      ) : (
-        <>
-          <Head>
-            {SeoDataSection({seoData: event.seo, slug: `eventi/${event.slug}`})}
-          </Head>
-          <HeaderBig
-            noButton
-            overlay="bg-gradient-to-tl from-secondary via-primary to-black opacity-80"
-            slopeSectionColor="text-gray-100"
-            backgroundImgSrc={
-              event.featuredImage && event.featuredImage.node.mediaItemUrl
-            }
-          />
-          <section className="relative w-full bg-gray-100 pt-16 pb-24 text-gray-800">
-            <div className="container mx-auto px-4">
-              <div className="relative lg:flex lg:flex-row">
-                <div className="relative -mt-96 mb-6 flex w-full min-w-0 flex-col break-words bg-white shadow-lg pb-24">
-                  <div className="px-6">
-                    <div className="mt-8 flex flex-wrap justify-center">
-                      <div className="flex items-center justify-center">
-                        <div className="mb-2 text-left text-gray-800">
-                          {event.startDate === event.endDate ? (
-                            <>
-                              <span className="block text-xl font-bold uppercase tracking-wide text-gray-600">
-                                <Date dateString={event.startDate} />
-                              </span>
-                              <span className="block text-lg font-bold text-yellow-500">
-                                h {event.startHour} - {event.endHour}
-                              </span>
-                            </>
-                          ) : (
-                            <>
-                              dal{' '}
-                              <span className="text-xl font-bold uppercase tracking-wide text-gray-600">
-                                <Date dateString={event.startDate} />
-                              </span>
-                              <br />
-                              al{' '}
-                              <span className="text-xl font-bold uppercase tracking-wide text-gray-600">
-                                <Date dateString={event.startDate} />
-                              </span>
-                              <span className="block text-lg font-bold text-yellow-500">
-                                h {getHour(event.startDate)} -{' '}
-                                {getHour(event.endDate)}
-                              </span>
-                            </>
-                          )}
+      {router.isFallback
+        ? (
+          <>
+            <Header href="/eventi">Eventi</Header>
+            <Head>
+              <title>Matarrese srl | Eventi</title>
+            </Head>
+            <main>
+              <H2 variant="secondary">Caricamento di tutti gli eventi</H2>
+            </main>
+          </>
+        )
+        : (
+          <>
+            <Head>
+              {SeoDataSection({
+                seoData: event.seo,
+                slug: `eventi/${event.slug}`,
+              })}
+            </Head>
+            <HeaderBig
+              noButton
+              overlay="bg-gradient-to-tl from-secondary via-primary to-black opacity-80"
+              slopeSectionColor="text-gray-100"
+              backgroundImgSrc={event.featuredImage &&
+                event.featuredImage.node.mediaItemUrl}
+            />
+            <section className="relative w-full bg-gray-100 pt-16 pb-24 text-gray-800">
+              <div className="container mx-auto px-4">
+                <div className="relative lg:flex lg:flex-row">
+                  <div className="relative -mt-96 mb-6 flex w-full min-w-0 flex-col break-words bg-white shadow-lg pb-24">
+                    <div className="px-6">
+                      <div className="mt-8 flex flex-wrap justify-center">
+                        <div className="flex items-center justify-center">
+                          <div className="mb-2 text-left text-gray-800">
+                            {event.startDate === event.endDate
+                              ? (
+                                <>
+                                  <span className="block text-xl font-bold uppercase tracking-wide text-gray-600">
+                                    <time>{event.startDate}</time>
+                                    {/* <Date dateString={event.startDate} />*/}
+                                  </span>
+                                  <span className="block text-lg font-bold text-yellow-500">
+                                    h {event.startHour} - {event.endHour}
+                                  </span>
+                                </>
+                              )
+                              : (
+                                <>
+                                  dal{" "}
+                                  <span className="text-xl font-bold uppercase tracking-wide text-gray-600">
+                                    <time>{event.startDate}</time>
+                                    {/* <Date dateString={event.startDate} />*/}
+                                  </span>
+                                  <br />
+                                  al{" "}
+                                  <span className="text-xl font-bold uppercase tracking-wide text-gray-600">
+                                    <time>{event.endDate}</time>
+                                    {/* <Date dateString={event.startDate} />*/}
+                                  </span>
+                                  <span className="block text-lg font-bold text-yellow-500">
+                                    h {event.startHour} - {event.endHour}
+                                  </span>
+                                </>
+                              )}
+                          </div>
                         </div>
-                      </div>
-                      {!event.isPast ? (
-                        <div className="w-4/12 self-center px-4 text-right">
-                          <FormModal
-                            buttonText="Partecipa"
-                            buttonClassName="px-4 py-2 mb-1 text-xs font-bold text-white uppercase transition-all duration-150 ease-linear bg-gradient-to-tl from-primary to-secondary rounded shadow outline-none active:bg-yellow-500 hover:shadow-md focus:outline-none sm:mr-2"
-                            type="reservation"
-                            title={event.title}
-                            withButton
-                          />
-                        </div>
-                      ) : null}
-                    </div>
-                    <div className="mt-12">
-                      <H3 className="mb-2 text-center" variant="secondary">
-                        {event.title}
-                      </H3>
-                      {/* FIXME: with the latest version of the event calendar WP
-                          ql-event can't fetch venue and organization 
-                          seems like it's a problem other doesn't have
-                      */}
-                      {/*  {event.venue && ( */}
-                      {/*   <div className="mb-2 mt-0 text-gray-400 text-sm font-bold leading-normal uppercase"> */}
-                      {/*     <i className="fas fa-map-marker-alt mr-2 text-gray-400 text-lg"></i>{' '} */}
-                      {/*     <a */}
-                      {/*       href={`https://maps.google.com/?q=${addressToMapsLink( */}
-                      {/*         event.venue.title, */}
-                      {/*         event.venue.city, */}
-                      {/*         event.venue.address, */}
-                      {/*       )}`} */}
-                      {/*       target="_blank" */}
-                      {/*       rel="noreferrer" */}
-                      {/*     > */}
-                      {/*       {event.venue.title} - {event.venue.address},{' '} */}
-                      {/*       {event.venue.city} */}
-                      {/*     </a> */}
-                      {/*   </div> */}
-                      {/* )} */}
-                      {/* {event.organizers && ( */}
-                      {/*   <div className="mb-2 mt-10 text-gray-600"> */}
-                      {/*     <i className="fas fa-briefcase mr-2 text-gray-400 text-lg"></i> */}
-                      {/*     Organizzato da:{' '} */}
-                      {/*     <a href={event.organizers?.nodes[0].link}> */}
-                      {/*       {event.organizers?.nodes[0].title} */}
-                      {/*     </a> */}
-                      {/*   </div> */}
-                      {/* )}  */}
-                    </div>
-
-                    <div className="mt-10 border-t border-gray-200 py-10">
-                      <div className="flex flex-wrap justify-center">
-                        <div className="w-full px-4 lg:w-9/12">
-                          <EventBody content={event.content} />
-                          {!event.isPast ? (
-                            <div className="flex items-center justify-end py-10">
-                              <Paragraph className="mr-8 font-medium">
-                                Prenota subito il tuo posto all&apos;evento.
-                                Clicca su &ldquo;Partecipa&rdquo;
-                              </Paragraph>
+                        {!event.isPast
+                          ? (
+                            <div className="w-4/12 self-center px-4 text-right">
                               <FormModal
                                 buttonText="Partecipa"
                                 buttonClassName="px-4 py-2 mb-1 text-xs font-bold text-white uppercase transition-all duration-150 ease-linear bg-gradient-to-tl from-primary to-secondary rounded shadow outline-none active:bg-yellow-500 hover:shadow-md focus:outline-none sm:mr-2"
@@ -162,45 +117,106 @@ export default function Events({event}) {
                                 withButton
                               />
                             </div>
-                          ) : null}
+                          )
+                          : null}
+                      </div>
+                      <div className="mt-12">
+                        <H3 className="mb-2 text-center" variant="secondary">
+                          {event.title}
+                        </H3>
+                        {
+                          /* FIXME: with the latest version of the event calendar WP
+                          ql-event can't fetch venue and organization
+                          seems like it's a problem other doesn't have
+                      */
+                        }
+                        {/*  {event.venue && ( */}
+                        {/*   <div className="mb-2 mt-0 text-gray-400 text-sm font-bold leading-normal uppercase"> */}
+                        {/*     <i className="fas fa-map-marker-alt mr-2 text-gray-400 text-lg"></i>{' '} */}
+                        {/*     <a */}
+                        {/*       href={`https://maps.google.com/?q=${addressToMapsLink( */}
+                        {/*         event.venue.title, */}
+                        {/*         event.venue.city, */}
+                        {/*         event.venue.address, */}
+                        {/*       )}`} */}
+                        {/*       target="_blank" */}
+                        {/*       rel="noreferrer" */}
+                        {/*     > */}
+                        {/*       {event.venue.title} - {event.venue.address},{' '} */}
+                        {/*       {event.venue.city} */}
+                        {/*     </a> */}
+                        {/*   </div> */}
+                        {/* )} */}
+                        {/* {event.organizers && ( */}
+                        {/*   <div className="mb-2 mt-10 text-gray-600"> */}
+                        {/*     <i className="fas fa-briefcase mr-2 text-gray-400 text-lg"></i> */}
+                        {/*     Organizzato da:{' '} */}
+                        {/*     <a href={event.organizers?.nodes[0].link}> */}
+                        {/*       {event.organizers?.nodes[0].title} */}
+                        {/*     </a> */}
+                        {/*   </div> */}
+                        {/* )}  */}
+                      </div>
+
+                      <div className="mt-10 border-t border-gray-200 py-10">
+                        <div className="flex flex-wrap justify-center">
+                          <div className="w-full px-4 lg:w-9/12">
+                            <EventBody content={event.content} />
+                            {!event.isPast
+                              ? (
+                                <div className="flex items-center justify-end py-10">
+                                  <Paragraph className="mr-8 font-medium">
+                                    Prenota subito il tuo posto all&apos;evento.
+                                    Clicca su &ldquo;Partecipa&rdquo;
+                                  </Paragraph>
+                                  <FormModal
+                                    buttonText="Partecipa"
+                                    buttonClassName="px-4 py-2 mb-1 text-xs font-bold text-white uppercase transition-all duration-150 ease-linear bg-gradient-to-tl from-primary to-secondary rounded shadow outline-none active:bg-yellow-500 hover:shadow-md focus:outline-none sm:mr-2"
+                                    type="reservation"
+                                    title={event.title}
+                                    withButton
+                                  />
+                                </div>
+                              )
+                              : null}
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
+                  <SocialShareBar route={router.asPath} title={event.title} />
                 </div>
-                <SocialShareBar route={router.asPath} title={event.title} />
+                <div className="flex justify-center">
+                  <Link href="/eventi">
+                    <a className="mt-10 rounded bg-white p-4 text-center uppercase shadow-lg hover:shadow-sm">
+                      Eventi
+                    </a>
+                  </Link>
+                </div>
               </div>
-              <div className="flex justify-center">
-                <Link href="/eventi">
-                  <a className="mt-10 rounded bg-white p-4 text-center uppercase shadow-lg hover:shadow-sm">
-                    Eventi
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </section>
-        </>
-      )}
+            </section>
+          </>
+        )}
     </Layout>
-  )
+  );
 }
 
-export async function getStaticProps({params}) {
-  const event = await getEvent(params.slug)
+export async function getStaticProps({ params }) {
+  const event = await getEvent(params.slug);
 
   return {
     props: {
       event,
     },
     revalidate: 60 * 60 * 24,
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const allEvents = await getAllEventsWithSlug()
+  const allEvents = await getAllEventsWithSlug();
 
   return {
-    paths: allEvents.edges.map(({node}) => `/eventi/${node.slug}`) || [],
+    paths: allEvents.edges.map(({ node }) => `/eventi/${node.slug}`) || [],
     fallback: true,
-  }
+  };
 }
